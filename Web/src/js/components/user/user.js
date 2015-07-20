a4d1a91d5819165868e2a90fa8380311b1bcc939
Ilana Sufrin <ilanasufrin@gmail.com>
2015-07-20 01:29:32 -0400

    only makes request for user id from URL. see console for results

--- Web/src/js/components/user/user.js
+++ Web/src/js/components/user/user.js
@@ -1,7 +1,5 @@
 /** @jsx React.DOM */
 var React = require('react');
-// var ParseReact = require('parse-react');
-
 
 var User = React.createClass({
   mixins: [ParseReact.Mixin],
@@ -16,8 +14,29 @@ var User = React.createClass({
   // },
 
   observe: function() {
+    var gifList = Parse.Object.extend("Assets");
+    var query = new Parse.Query(gifList);
+    query.equalTo("account_id", this.props.params.userID);
+
+    console.log("QUERY", query);
+
+    query.find({
+      success: function(results) {
+        console.log("Successfully retrieved ", results.length, " images.");
+        // Do something with the returned Parse.Object values
+        for (var i = 0; i < results.length; i++) {
+          var object = results[i];
+          console.log(object.id, ' - ', object.get('asset'));
+        }
+      },
+      error: function(error) {
+        console.log("Error: ", error.code, " " + error.message);
+      }
+    });
+
     return {
-      users: (new Parse.Query('Users')).ascending('createdAt')
+      users: (new Parse.Query('Users')).ascending('createdAt'),
+      gifs: (new Parse.Query('Assets')).ascending('createdAt')
     };
   },
   render: function () {
@@ -27,6 +46,18 @@ var User = React.createClass({
       <div className="User">
         <h1>User id: {this.props.params.userID}</h1>
         <h2>{this.data.users}</h2>
+        <hr/>
+        <ul>
+         {this.data.users.map(function(c) {
+           return <li>{c}</li>;
+         })}
+        </ul>
+        <hr/>
+        <ul>
+         {this.data.gifs.map(function(g) {
+           return <li><img src='{g}'/></li>;
+         })}
+        </ul>
       </div>
     );
   }
