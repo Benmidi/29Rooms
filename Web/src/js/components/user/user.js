4d7c673d73aa546673618771934960e50f8e88a1
Ilana Sufrin <ilanasufrin@gmail.com>
2015-07-20 19:37:01 -0400

    css and messing around with this.state

--- Web/src/js/components/user/user.js
+++ Web/src/js/components/user/user.js
@@ -3,17 +3,19 @@ var React = require('react');
 
 var User = React.createClass({
   mixins: [ParseReact.Mixin],
-  // getInitialState: function () {
-  // //  return AuthStore.getState();
-  // },
+   getInitialState: function () {
+     return {data: []};
+   },
   // setStateOnAuth: function (loggedIn) {
   // //  this.setState(AuthStore.getState());
   // },
-  // componentWillMount: function () {
-  //  // AuthStore.authOnChangeHeader(this.setStateOnAuth);
-  // },
+  componentWillMount: function () {
+   // AuthStore.authOnChangeHeader(this.setStateOnAuth);
+   console.log("componentWillMount");
+  },
 
   observe: function() {
+    console.log("OBSERVE");
     var gifList = Parse.Object.extend("Assets");
     var query = new Parse.Query(gifList);
     query.equalTo("account_id", this.props.params.userID);
@@ -34,14 +36,27 @@ var User = React.createClass({
       }
     });
 
+    this.setState({
+      users: (new Parse.Query('Users')).ascending('createdAt'),
+      gifs: (new Parse.Query('Assets')).equalTo("account_id", this.props.params.userID).ascending('createdAt'),
+      data: "TESTSTRING"
+    });
+
     return {
       users: (new Parse.Query('Users')).ascending('createdAt'),
-      gifs: (new Parse.Query('Assets')).ascending('createdAt')
+      gifs: (new Parse.Query('Assets')).equalTo("account_id", this.props.params.userID).ascending('createdAt')
     };
   },
   render: function () {
+    var hasAssets = (this.data.gifs.length > 0) ? !!this.data.gifs : false;
+    console.log("HAS ASSETS", hasAssets);
     console.log("hello");
     console.log(this.data.users);
+    console.log("GIFS");
+    console.log(this.data.gifs);
+   //console.log("this.state.gifs", this.state.gifs);
+   console.log(this.state.data);
+
     return (
       <div className="User">
         <h1>User id: {this.props.params.userID}</h1>
@@ -55,7 +70,13 @@ var User = React.createClass({
         <hr/>
         <ul>
          {this.data.gifs.map(function(g) {
-           return <li><img src='{g}'/></li>;
+            console.log("ASSETS");
+            console.log(g);
+           if (hasAssets) { 
+             return <div><h3>CHECKPOINT {g.checkpoint}</h3><li><img src={g.asset._url} className='checkpointImages'/></li></div>;
+           } else {
+            return <h2>No photos!</h2>
+           }
          })}
         </ul>
       </div>
